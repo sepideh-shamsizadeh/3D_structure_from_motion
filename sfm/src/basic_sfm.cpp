7224a15c40dd2c46ae378d5f891ff975d1f358e6
sepideh <sepideh.shamsizadeh@gmail.com>
2022-05-11 10:10:36 +0200

    task 4, AddResidualBlock

--- sfm/src/basic_sfm.cpp
+++ sfm/src/basic_sfm.cpp
@@ -13,6 +13,52 @@ using namespace std;
 
 struct ReprojectionError
 {
+    ReprojectionError(double observed_x, double observed_y)
+            : observed_x(observed_x), observed_y(observed_y) {}
+
+    template <typename T>
+    bool operator()(const T* const camera,
+                    const T* const point,
+                    T* residuals) const {
+        // camera[0,1,2] are the angle-axis rotation.
+        T p[3];
+        ceres::AngleAxisRotatePoint(camera, point, p);
+        // camera[3,4,5] are the translation.
+        p[0] += camera[3]; p[1] += camera[4]; p[2] += camera[5];
+
+        // Compute the center of distortion. The sign change comes from
+        // the camera model that Noah Snavely's Bundler assumes, whereby
+        // the camera coordinate system has a negative z axis.
+        T xp = - p[0] / p[2];
+        T yp = - p[1] / p[2];
+
+        // Apply second and fourth order radial distortion.
+        const T& l1 = camera[7];
+        const T& l2 = camera[8];
+        T r2 = xp*xp + yp*yp;
+        T distortion = 1.0 + r2  * (l1 + l2  * r2);
+
+        // Compute final projected point position.
+        const T& focal = camera[6];
+        T predicted_x = focal * distortion * xp;
+        T predicted_y = focal * distortion * yp;
+
+        // The error is the difference between the predicted and observed position.
+        residuals[0] = predicted_x - T(observed_x);
+        residuals[1] = predicted_y - T(observed_y);
+        return true;
+    }
+
+    // Factory to hide the construction of the CostFunction object from
+    // the client code.
+    static ceres::CostFunction* Create(const double observed_x,
+                                       const double observed_y) {
+        return (new ceres::AutoDiffCostFunction<ReprojectionError, 2, 9, 3>(
+                new ReprojectionError(observed_x, observed_y)));
+    }
+
+    double observed_x;
+    double observed_y;
     //////////////////////////// Code to be completed (5/5) //////////////////////////////////
     // This class should include an auto-differentiable cost function (see Ceres Solver docs).
     // Remember that we are dealing with a normalized, canonical camera:
@@ -525,7 +571,7 @@ void BasicSfM::solve()
         cv::Mat E = cv::findEssentialMat(points0, points1, intrinsics_matrix, cv::RANSAC, 0.99, 1, inlier_mask_E);
         cv::findHomography(points0, points1, inlier_mask_H, cv::LMEDS);
         int e = cv::sum(inlier_mask_E)[0];
-        int h =cv::sum(inlier_mask_H)[0];
+        int h = cv::sum(inlier_mask_H)[0];
 
         if( e>h){
             cv::recoverPose(E, points0, points1, intrinsics_matrix, init_r_mat,init_t, inlier_mask_E);
@@ -764,7 +810,15 @@ void BasicSfM::bundleAdjustmentIter( int new_cam_idx )
             //.. check if this observation has bem already registered (bot checking camera pose and point pose)
             if( pose_optim_iter_[pose_index_[i_obs]] > 0 && pts_optim_iter_[point_index_[i_obs]] > 0 )
             {
-
+                ceres::CostFunction* cost_function =
+                        ReprojectionError::Create(
+                                observations_[2 * i_obs + 0],
+                                observations_[2 * i_obs + 1]);
+                
+                problem.AddResidualBlock(cost_function,
+                                         new ceres::CauchyLoss(2*max_reproj_err_),
+                                         parameters_.data() + camera_block_size_ * i_obs,
+                                         parameters_.data() + point_block_size_ * i_obs);
                 //////////////////////////// Code to be completed (4/5) /////////////////////////////////
                 //.. in case, add a residual block inside the Ceres solver problem.
                 // You should define a suitable functor (i.e., see the ReprojectionError struct at the
